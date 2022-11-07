2b60051b935a54e990c7356ec590bf64ee61e5ba
Anna Sobova <anna_sobova@ukr.net>
2022-11-08 01:20:37 +0200

    fixed

--- src/components/Phonebook/Button/ButtonDelete/ButtonDelete.styled.jsx
+++ src/components/Phonebook/Button/ButtonDelete/ButtonDelete.styled.jsx
@@ -8,10 +8,10 @@ const ButtonDeleteStyle = styled.button`
   cursor: pointer;
   transition: color 250ms linear, background-color 250ms linear;
 
-  &: hover {
+  /* &: hover {
     background-color: white;
     color: black;
-  }
+  } */
 `;
 
 export default ButtonDeleteStyle ;
