94ac103369c16572fd3625f33d1b7d09a83547f5
Anna Sobova <anna_sobova@ukr.net>
2022-11-08 01:10:17 +0200

    commit with form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,15 @@
 import { Component } from 'react';
-// import { nanoid } from 'nanoid/non-secure';
+import { nanoid } from 'nanoid/non-secure';
 import Container from './container/Container';
 import Phonebook from './Phonebook/Phonebook';
+import Contacts from './Phonebook/Contacts/Contacts';
+
 
 class App extends Component{
   state = {
     contacts: [],
-    name: ''
+
+    filter: '',
   }
    
   handleChange = e => {
@@ -16,20 +19,63 @@ class App extends Component{
     });
   };
    
+  formSubmitHandle = data => {
+    const id = nanoid();
+    if (
+      this.state.contacts.filter(contact => contact.name === data.name).length >
+      0
+    ) {
+      alert(`${data.name} is already in contacts`);
+      return;
+    }
+    this.setState({
+      contacts: [
+        ...this.state.contacts,
+        {
+          name: data.name,
+          number: data.number,
+          id: id,
+        },
+      ],
+    });
+  };
+
+
+  onClickDelete = id => {
+    this.setState({
+      contacts: this.state.contacts.filter(contact => contact.id !== id),
+    });
+  };
 
 render() {
-
+  const normolizeFilter = this.state.filter.toLowerCase();
+  const visibleContacts = this.state.contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normolizeFilter)
+  );
 return(
   <Container title="Phonebook">
      
- <Phonebook
- onChange={this.handleChange}
- 
- />
-
+     <Phonebook
+          onChange={this.handleChange}
+          onSubmit={this.formSubmitHandle}
+          contactsList={visibleContacts}
+          notEmptyList={this.state.contacts.length}
+          valueFilter={this.state.filter}
+        />
+          {this.state.contacts.length > 0 ? (
+          <Contacts
+            name="Contacts"
+            contactsList={visibleContacts}
+            onChange={this.handleChange}
+            value={this.state.filter}
+            onClickDelete={this.onClickDelete}
+          />
+        ) : (
+          <p>Phonebook empty</p>
+        )}
   </Container>
   )
-};
+ }
 }
 
 export default App;
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,9 +1,11 @@
 import {Component }from 'react';
 import FormPhonebook from './Form/Form';
 import InputName from './Input/InputName';
+import InputNumber from './Input/InputNumber';
+import LabelPhoneBook from './Label/Label.styled';
+import ButtonSubmit from './Button/ButtonSubmit';
 
-
-class Phonebook extends Component{
+class Phonebook extends Component{ 
     state = {
         name: '',
         number: '',
@@ -13,31 +15,34 @@ class Phonebook extends Component{
         const{name,value}=event.currentTarget;
         this.setState({[name]: value});
       };
-
+        reset=()=>{
+        this.setState({name:'',number:''});
+      }
        
-      handleSubmit=event=>{
+      clickOnBtnSubmit=event=>{
         event.preventDefault();
-        this.props.OnSubmit(this.state);
+        this.props.onSubmit(this.state);
         this.reset();
       };
      
-      reset=()=>{
-        this.setState({name:'',number:''});
-      }
+      
 
        render(){
         return(
             <>
             <FormPhonebook onSubmit={this.clickOnBtnSubmit}>
-              <InputName value={this.state.name} onChange={this.handleChange}/>
+                <LabelPhoneBook title="Name">     
+            <InputName value={this.state.name} onChange={this.handleChange}/>
+            </LabelPhoneBook>
+            <LabelPhoneBook title="Number">
+            <InputNumber value={this.state.number} onChange={this.handleChange}/>
+            </LabelPhoneBook>
+           <ButtonSubmit text="Add contact"/>
             </FormPhonebook>
             </>
-        )
+        );
        }
-
-      
-
-};
+    }
 
 export default Phonebook;
 
